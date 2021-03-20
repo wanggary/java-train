b439b2505447fb7a152bf39c68cdb7cf4afbe04c
wsj <wsj_freedom@163.com>
2021-03-21 01:22:15 +0800

    week1

---
+++ week1/javaTrainCamp/src/main/java/com/wsj/train/CustomClassLoader.java
@@ -0,0 +1,62 @@
+package com.wsj.train;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class CustomClassLoader extends ClassLoader {
+    public static void main(String[] args) {
+        try {
+            new CustomClassLoader().findClass("Hello").newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] helloBytes = getHelloBytes("src\\main\\resources\\Hello.xlass");
+
+        return defineClass(name, helloBytes, 0, helloBytes.length);
+    }
+
+    public byte[] getHelloBytes(String file) {
+        FileInputStream in = null;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] bytes = null;
+        try {
+            in = new FileInputStream(file);
+            byte[] buf = new byte[1024];
+            int len = 0;
+            while ((len = in.read(buf)) != -1) {
+                out.write(buf, 0, len);
+            }
+            bytes = out.toByteArray();
+
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i]  = (byte) (255 - bytes[i]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return bytes;
+    }
+}
